c82e4612916bf7065ff3772ec6f9f9274495c84a
mirem_12 <mirem.isildak@ogr.sakarya.edu.tr>
2021-02-15 14:05:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                MyDictionary<string> urunler = new MyDictionary<string>();
+                urunler.Add("Bardak");
+                urunler.Add("Kase");
+                Console.WriteLine(urunler.Length);
+                urunler.Add("Tabak");
+                Console.WriteLine(urunler.Length);
+
+            
+        }
+    }
+}
